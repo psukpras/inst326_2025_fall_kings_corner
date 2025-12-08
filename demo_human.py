d2ecd520f75da4faf3b1edaa7e4db8428ea0d4d8
asooketh <asooketh@gmail.com>
2025-12-07 21:11:21 -0500

    Add files via upload

---
+++ demo_human.py
@@ -0,0 +1,20 @@
+from kings_corner import HumanPlayer
+player = HumanPlayer("Demo_Player")
+player.hand = [('K', 'Red'), (5, 'Black'), (2, 'Red'), ('Q', 'Black')]
+state = {
+    'piles': {
+        'N': [(6, 'Red')],
+        'S': [],
+        'E': [(4, 'Black')],
+        'W': [],
+        'NE': [],
+        'NW': [],
+        'SE': [],
+        'SW': []
+    },
+    'draw_pile': [('A', 'Red'), (3, 'Black')]
+}
+print("\nTaking a turn...")
+print("(Choose options 1-4 to test different actions)")
+action = player.turn(state)
+print(f"\nFinal action result: {action}")
