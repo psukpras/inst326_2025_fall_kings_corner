7d19963187ac9790e01e9ea0d83b6c188ba20f7c
Michael Huang <mhuang2@umd.edu>
2025-12-11 13:38:19 -0500

    Spacing

--- kings_corner.py
+++ kings_corner.py
@@ -418,6 +418,7 @@ class ComputerPlayer(Player):
         # Strategy 5: End turn
         print(f"{self.name} chooses to end turn.")
         return "end"
+
 """ Edit by Michael 12.06.2025 """
 def player_turn(player_hand, play_piles, draw_pile, player):
     """
